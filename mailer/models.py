391c95ab992071f46a1cf41eb40e2b076f715e1b
Nazarova Elena <nazzarova.in@gmail.com>
2025-03-31 11:30:55 +0300

    Adding the newsletter by the button.

--- mailer/models.py
+++ mailer/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.mail import send_mail
+from django.utils.timezone import now
 
 
 class Recipient(models.Model):
@@ -19,8 +21,31 @@ class EmailCompany(models.Model):
     message = models.TextField(help_text="Enter the text of the message ")
     recipients = models.ManyToManyField(Recipient, related_name='emails', help_text="Choose recipients")
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    sent_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+    def send_emails(self):
+        recipient_emails = [recipient.email for recipient in self.recipients.all()]
+
+        if not recipient_emails:
+            return "No recipients to send emails to."
+
+        send_mail(
+            subject=self.title,
+            message=self.message,
+            from_email="nazzarova.in@gmail.com",
+            recipient_list=recipient_emails,
+            fail_silently=False,
+        )
+
+        self.status = 'sent'
+        self.sent_at = now()
+        self.save()
+
+        return f"Emails sent to {len(recipient_emails)} recipients."
+
+
 
 
--- mailer/views.py
+++ mailer/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import EmailCompany
 from .serializers import EmailCompanySerializer
@@ -7,5 +9,18 @@ from .serializers import EmailCompanySerializer
 class EmailCompanyViewSet(viewsets.ModelViewSet):
     queryset = EmailCompany.objects.prefetch_related('recipients').all()
     serializer_class = EmailCompanySerializer
+
+    @action(detail=True, methods=['post'])
+    def send_company(self, request, pk=None):
+        email_company = self.get_object()
+
+        if email_company.recipients.exists() and email_company.status =="draft":
+            email_company.send_emails()
+            return Response({"message": "Emails sent!"})
+
+        return Response({"error": "Cannot send emails!"}, status=400)
+
+
+
 
 
